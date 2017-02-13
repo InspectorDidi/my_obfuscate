e68aec3b074b4cc952249ee7dba24395177c2186
Jose Luis Salas <josacar@users.noreply.github.com>
2017-02-13 16:54:50 +0100

    [WIP] Converting to Crystal

--- spec/my_obfuscate/config_applicator_spec.cr
+++ spec/my_obfuscate/config_applicator_spec.cr
@@ -6,25 +6,25 @@ describe MyObfuscate::ConfigApplicator do
     it "should work on email addresses" do
       100.times do
         new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else"], {:a => {:type => :email}}, [:a, :b])
-        expect(new_row.length).to eq(2)
+        expect(new_row.size).to eq(2)
         expect(new_row.first).to match(/^[\w\.]+\@(\w+\.){2,3}[a-f0-9]{5}\.example\.com$/)
       end
     end
 
     it "should work on strings" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "something crazy"], {:b => {:type => :string, :length => 7}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
-      expect(new_row[1].length).to eq(7)
+      expect(new_row.size).to eq(3)
+      expect(new_row[1].size).to eq(7)
       expect(new_row[1]).not_to eq("something_else")
     end
 
     describe "conditional directives" do
       it "should honor :unless conditionals" do
-        new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a=> {:type => :fixed, :string => "123", :unless => lambda { |row| row[:a] == "blah" }}}, [:a, :b, :c])
+        new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a=> {:type => :fixed, :string => "123", :unless => Proc(Hash(Symbol, String),Bool).new { |row| row[:a] == "blah" }}}, [:a, :b, :c])
         expect(new_row[0]).not_to eq("123")
         expect(new_row[0]).to eq("blah")
 
-        new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a=> {:type => :fixed, :string => "123", :unless => lambda { |row| row[:a] == "not blah" }}}, [:a, :b, :c])
+        new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a=> {:type => :fixed, :string => "123", :unless => Proc(Hash(Symbol, String), Bool).new { |row| row[:a] == "not blah" }}}, [:a, :b, :c])
         expect(new_row[0]).to eq("123")
 
         new_row = MyObfuscate::ConfigApplicator.apply_table_config([nil, "something_else", "5"], {:a=> {:type => :fixed, :string => "123", :unless => :nil}, :b=> {:type => :fixed, :string => "123", :unless => :nil}}, [:a, :b, :c])
@@ -80,26 +80,26 @@ describe MyObfuscate::ConfigApplicator do
 
     it "should be able to generate random integers in ranges" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:c => {:type => :integer, :between => 10..100}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[2].to_i.to_s).to eq(new_row[2]) # It should be an integer.
       expect(new_row[2]).not_to eq("5")
     end
 
     it "should be able to substitute fixed strings" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => {:type => :fixed, :string => "hello"}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[1]).to eq("hello")
     end
 
     it "should be able to substitute a proc that returns a string" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => {:type => :fixed, :string => proc { "Hello World" }}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[1]).to eq("Hello World")
     end
 
     it "should provide the row to the proc" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => {:type => :fixed, :string => proc { |a| a[:b] }}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[1]).to eq("something_else")
     end
 
@@ -109,7 +109,7 @@ describe MyObfuscate::ConfigApplicator do
       guard = 0
       while !looking_for.empty? && guard < 1000
         new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => {:type => :fixed, :one_of => ["hello", "world"]}}, [:a, :b, :c])
-        expect(new_row.length).to eq(3)
+        expect(new_row.size).to eq(3)
         expect(original_looking_for).to contain(new_row[0])
         looking_for.delete new_row[0]
         guard += 1
@@ -119,20 +119,20 @@ describe MyObfuscate::ConfigApplicator do
 
     it "should treat a symbol in the column definition as an implicit { :type => symbol }" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => :null, :a => :keep}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).to eq("blah")
       expect(new_row[1]).to eq(nil)
     end
 
     it "should be able to set things NULL" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => {:type => :null}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[1]).to eq(nil)
     end
 
     it "should be able to :keep the value the same" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => {:type => :keep}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[1]).to eq("something_else")
     end
 
@@ -140,7 +140,7 @@ describe MyObfuscate::ConfigApplicator do
       # $stderr = error_output = StringIO.new
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:b => {:type => :unknown_type}}, [:a, :b, :c])
       # $stderr = STDERR
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[1]).to eq("something_else")
       error_output.rewind
       expect(error_output.read).to match(/Keeping a column value by.*?unknown_type/)
@@ -148,7 +148,7 @@ describe MyObfuscate::ConfigApplicator do
 
     it "should be able to substitute lorem ipsum text" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :lorem, :b => {:type => :lorem, :number => 2}}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
       expect(new_row[0]).not_to match(/\w\.(?!\Z)/)
       expect(new_row[1]).not_to eq("something_else")
@@ -157,75 +157,75 @@ describe MyObfuscate::ConfigApplicator do
 
     it "should be able to generate an :company" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["Smith and Sons", "something_else", "5"], {:a => :company}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("Smith and Sons")
       expect(new_row[0]).to match(/\w+/)
     end
 
     it "should be able to generate an :url" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["http://mystuff.blogger.com", "something_else", "5"], {:a => :url}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("http://mystuff.blogger.com")
       expect(new_row[0]).to match(/http:\/\/\w+/)
     end
 
     it "should be able to generate an :ipv4" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["1.2.3.4", "something_else", "5"], {:a => :ipv4}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("1.2.3.4")
       expect(new_row[0]).to match(/\d+\.\d+\.\d+\.\d+/)
     end
 
     it "should be able to generate an :ipv6" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["fe80:0000:0000:0000:0202:b3ff:fe1e:8329", "something_else", "5"], {:a => :ipv6}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("fe80:0000:0000:0000:0202:b3ff:fe1e:8329")
       expect(new_row[0]).to match(/[0-9a-f:]+/)
     end
 
     it "should be able to generate an :address" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :address}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
       expect(new_row[0]).to match(/\d+ \w/)
     end
 
     it "should be able to generate a :name" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :name}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
       expect(new_row[0]).to match(/ /)
     end
 
     it "should be able to generate just a street address" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :street_address}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
       expect(new_row[0]).to match(/\d+ \w/)
     end
 
     it "should be able to generate a city" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :city}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
     end
 
     it "should be able to generate a state" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :state}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
     end
 
     it "should be able to generate a zip code" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :zip_code}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
       expect(new_row[0]).to match(/\d+/)
     end
 
     it "should be able to generate a phone number" do
       new_row = MyObfuscate::ConfigApplicator.apply_table_config(["blah", "something_else", "5"], {:a => :phone}, [:a, :b, :c])
-      expect(new_row.length).to eq(3)
+      expect(new_row.size).to eq(3)
       expect(new_row[0]).not_to eq("blah")
       expect(new_row[0]).to match(/\d+/)
     end
--- src/my_obfuscate/config_applicator.cr
+++ src/my_obfuscate/config_applicator.cr
@@ -1,7 +1,7 @@
 class MyObfuscate
   class ConfigApplicator
 
-    def self.apply_table_config(row : Array(String), table_config, columns : Array(Symbol))
+    def self.apply_table_config(row, table_config, columns : Array(Symbol))
       return row unless table_config.is_a?(Hash)
       row_hash = row_as_hash(row, columns)
 
@@ -22,57 +22,59 @@ class MyObfuscate
         end
 
         if definition[:skip_regexes]
-          next if definition[:skip_regexes].any? {|regex| row[index] =~ regex}
+          skip_regexes = definition[:skip_regexes].as(Array(Regex))
+          next if skip_regexes.any? { |regex| row[index] =~ regex }
         end
 
-        row[index.to_i] = case definition[:type]
+        row[index] = case definition[:type]
           when :email
-            md5 = Digest::MD5.hexdigest(rand.to_s)[0...5]
-            clean_quotes("#{FFaker::Internet.email}.#{md5}.example.com")
+            md5 = Crypto::MD5.hex_digest(rand.to_s)[0...5]
+            clean_quotes("#{Faker::Internet.email}.#{md5}.example.com")
           when :string
-            random_string(definition[:length] || 30, definition[:chars] || SENSIBLE_CHARS)
+            random_string(definition[:length].as(Int32) || 30, definition[:chars].as(String) || SENSIBLE_CHARS)
           when :lorem
-            clean_bad_whitespace(clean_quotes(FFaker::Lorem.sentences(definition[:number] || 1).join(".  ")))
+            clean_bad_whitespace(clean_quotes(Faker::Lorem.sentences(definition[:number].as(Int32) || 1).join(".  ")))
           when :like_english
-            clean_quotes random_english_sentences(definition[:number] || 1)
+            clean_quotes random_english_sentences(definition[:number].as(Int32) || 1)
           when :name
-            clean_quotes(FFaker::Name.name)
+            clean_quotes(Faker::Name.name)
           when :first_name
-            clean_quotes(FFaker::Name.first_name)
+            clean_quotes(Faker::Name.first_name)
           when :last_name
-            clean_quotes(FFaker::Name.last_name)
+            clean_quotes(Faker::Name.last_name)
           when :address
-            clean_quotes("#{FFaker::AddressUS.street_address}\\n#{FFaker::AddressUS.city}, #{FFaker::AddressUS.state_abbr} #{FFaker::AddressUS.zip_code}")
+            clean_quotes("#{Faker::Address.street_address}\\n#{Faker::Address.city}, #{Faker::Address.state_abbr} #{Faker::Address.zip_code}")
           when :street_address
-            clean_bad_whitespace(clean_quotes(FFaker::AddressUS.street_address))
+            clean_bad_whitespace(clean_quotes(Faker::Address.street_address))
           when :secondary_address
-            clean_bad_whitespace(clean_quotes(FFaker::AddressUS.secondary_address))
+            clean_bad_whitespace(clean_quotes(Faker::Address.secondary_address))
           when :city
-            clean_quotes(FFaker::AddressUS.city)
+            clean_quotes(Faker::Address.city)
           when :state
-            clean_quotes FFaker::AddressUS.state_abbr
+            clean_quotes Faker::Address.state_abbr
           when :zip_code
-            FFaker::AddressUS.zip_code
+            Faker::Address.zip_code
           when :phone
-            clean_quotes FFaker::PhoneNumber.phone_number
+            clean_quotes Faker::PhoneNumber.phone_number
           when :company
-            clean_bad_whitespace(clean_quotes(FFaker::Company.name))
+            clean_bad_whitespace(clean_quotes(Faker::Company.name))
           when :ipv4
-            FFaker::Internet.ip_v4_address
+            Faker::Internet.ip_v4_address
           when :ipv6
-            # Inlined from FFaker because ffaker doesn't have ipv6.
-            @@ip_v6_space ||= (0..65535).to_a
-            container = (1..8).map{ |_| @@ip_v6_space.sample }
-            container.map{ |n| n.to_s(16) }.join(':')
+            Faker::Internet.ip_v6_address
           when :url
-            clean_bad_whitespace(FFaker::Internet.http_url)
+            clean_bad_whitespace(Faker::Internet.url)
           when :integer
-            random_integer(definition[:between] || (0..1000)).to_s
+            random_integer(definition[:between].as(Range(Int32, Int32)) || (0..1000)).to_s
           when :fixed
             if definition[:one_of]
-              definition[:one_of][(rand * definition[:one_of].length).to_i]
+              definition[:one_of].as(Array).sample
             else
-              definition[:string].is_a?(Proc) ? definition[:string].call(row_hash) : definition[:string]
+              if definition[:string].is_a?(Proc)
+                definition[:string].as(Proc).call(row_hash)
+              else
+                definition[:string]
+              end
             end
           when :null
             nil
@@ -81,56 +83,66 @@ class MyObfuscate
           else
             STDERR.puts "Keeping a column value by providing an unknown type (#{definition[:type]}) is deprecated.  Use :keep instead."
             row[index]
-        end
+          end.as(String)
       end
       row
     end
 
-    def self.row_as_hash(row : Array(Int32 | String), columns : Array(Symbol))
-      columns.zip(row).each_with_object({} of Symbol => String)  { |(name, value),m| m[name] = value }
+    def self.row_as_hash(row : Array(String | Nil), columns : Array(Symbol))
+      columns.zip(row).each_with_object({} of Symbol => (String | Nil))  { |(name, value),m| m[name] = value }
     end
 
-    def self.make_conditional_method(conditional_method : (Symbol | Proc), index, row)
-      return conditional_method unless conditional_method.is_a?(Symbol)
+    def self.make_conditional_method(conditional_method, index, row)
+      return conditional_method if conditional_method.is_a?(Proc)
+      raise "Error" unless conditional_method.is_a?(Symbol)
 
       if conditional_method == :blank
-        Proc(Hash(Symbol, String), Bool).new { |_| row[index].nil? || row[index] == "" }
+        Proc(Hash(Symbol, String | Nil), Bool).new { |_| row[index].nil? || row[index].empty? }
       elsif conditional_method == :nil
-        Proc(Hash(Symbol, String), Bool).new { |_| row[index].nil? }
+        Proc(Hash(Symbol, String | Nil), Bool).new { |_| row[index].nil? }
       else
         raise "Error" # TODO Check if this is right
       end
     end
 
-    def self.random_integer(between)
-      (between.min + (between.max - between.min) * rand).round
+    def self.random_integer(between : Range(Int32, Int32)) : Int32
+      (between.min + (between.max - between.min) * rand).round.as(Int32)
     end
 
-    def self.random_string(length_or_range, chars)
-      length_or_range = (length_or_range..length_or_range) if length_or_range.is_a?(Fixnum)
-      times = random_integer(length_or_range)
+    def self.random_string(length_or_range, chars : String)
+      range = if length_or_range.is_a?(Int32)
+                (length_or_range..length_or_range)
+              elsif length_or_range.is_a?(Range)
+                length_or_range
+              else
+                raise "Error"
+              end
+      times = random_integer(range)
       out = ""
-      times.times { out << chars[rand * chars.length] }
+      times.times { out += chars[(rand * chars.size).as(Int32)] }
       out
     end
 
-    def self.random_english_sentences(num)
+    def self.walker_method
       @@walker_method ||= begin
-        words, counts = [] of String, [] of String
-        File.read(File.expand_path(File.join(File.dirname(__FILE__), "data", "en_50K.txt"))).each_line do |line|
-          word, count = line.split(/\s+/)
-          words << word
-          counts << count.to_i
-        end
-        WalkerMethod.new(words, counts)
-      end
+                            words, counts = [] of String, [] of Int32
+                            File.read(File.expand_path(File.join(File.dirname(__FILE__), "data", "en_50K.txt"))).each_line do |line|
+                              word, count = line.split(/\s+/)
+                              words << word
+                              counts << count.to_i
+                            end
+                            WalkerMethod.new(words, counts)
+                          end
+    end
+
+    def self.random_english_sentences(num : Int32)
 
       sentences = [] of String
       num.times do
         words = [] of String
-        (3 + rand * 5).to_i.times { words << @@walker_method.random }
-        sentences << words.join(" ") + "."
-        sentences.last[0] = sentences.last[0].upcase
+        (3 + rand * 5).to_i.times { words << walker_method.random }
+        sentence = words.join(" ") + "."
+        sentences << sentence.capitalize
       end
       sentences.join(" ")
     end
